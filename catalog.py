6f91086fec06ee3d5b571a36b0318d7ec99c6c4c
myasul <yasulmatthew@gmail.com>
2018-04-10 11:25:06 +0800

    Changes made:     - Implement login feature using Google's OAuth2

--- catalog.py
+++ catalog.py
@@ -4,13 +4,15 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for
 
 # Imports for CRUD functionality
-from sqlalchemy import create_engine, asc, func, select, MetaData
+from sqlalchemy import create_engine, asc, func, select, MetaData, exc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Category, Item
 
 # Imports for OAuth2 functionality
 from oauth2client.client import flow_from_clientsecrets, FlowExchangeError
-from flask import make_response, session as log_session
+from flask import make_response, session as login_session
+import random
+import string
 import requests
 
 # Imports to implement nested JSON
@@ -19,6 +21,9 @@ import functools
 import json
 
 app = Flask(__name__)
+
+CLIENT_ID = json.loads(open('client_secret_google.json',
+                            'r').read())['web']['client_id']
 
 # Connect to the database and create a database session
 engine = create_engine('postgresql:///catalog')
@@ -54,6 +59,105 @@ def show_category_items_JSON(category_id):
 def show_category_item_JSON():
     item = get_item(category_id, item_id)
     return jsonify(Category_Item[item.serialize])
+
+
+# Create anti-forgery token
+@app.route("/login")
+def generate_token():
+    state = ''.join(
+        random.choice(string.ascii_uppercase + string.digits)
+        for x in range(32))
+    login_session['state'] = state
+    return login_session['state']
+
+
+# TODO - REFACTOR!
+# Provide login functionality using Google's OAuth2
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Obtain authorization code
+    code = request.data
+    print(code)
+
+    try:
+        #Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets(
+            'client_secret_google.json', scope='')
+        oauth_flow.redirect_uri = 'http://localhost:5000'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code'), 401)
+        response.headers['Content-type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid
+    token_url = 'https://www.googleapis.com/oauth2/v1/tokeninfo'
+    params = {'access_token': credentials.access_token}
+    content = requests.get(token_url, params=params)
+    result = content.json()
+    if result.get('error'):
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for the intended user
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match giver user ID"), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's"), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_access_token:
+        response = make_response(json.dumps("User is already connected."), 200)
+        response.headers['Content-type'] = 'application/json'
+        return response
+
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    result = requests.get(userinfo_url, params=params)
+    data = result.json()
+
+    login_session['username'] = data['name']
+    login_session['image'] = data['picture']
+    login_session['email'] = data['email']
+
+    # Check if user already exists. If not, add it in the database
+    user_id = get_userid(login_session['email'])
+    if not user_id:
+        user_id = create_user(login_session)
+    login_session['user_id'] = user_id
+
+    response = make_response(json.dumps("User successfully logged in!"), 200)
+    response.headers['Content-type'] = 'application/json'
+    return response
+
+    #del login_session['access_token']
+    #del login_session['gplus_id']
+    #del login_session['username']
+    #del login_session['email']
+    #del login_session['image']
+    #del login_session['user_id']
 
 
 # Show all categories
@@ -251,7 +355,27 @@ def get_all_categories_JSON():
     return read(query)
 
 
+def create_user(login_session):
+    new_user = User(
+        name=login_session['username'],
+        email=login_session['email'],
+        image=login_session['image'])
+    session.add(new_user)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def get_userid(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except exc.SQLAlchemyError:
+        return None
+
+
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.config['JSON_SORT_KEYS'] = False
     app.run(host='0.0.0.0', port=5000)
--- static/js/category.js
+++ static/js/category.js
@@ -2,15 +2,68 @@
 
 $("[data-popup-open]").click(function(event) {
     targeted_popup_class = $(this).attr("data-popup-open");
-    $(`[data-popup=${targeted_popup_class}`).fadeIn(350);
+    $(`[data-popup=${targeted_popup_class}]`).fadeIn(350);
     event.preventDefault();
 });
 
 $("[data-popup-close]").click(function(event) {
     targeted_popup_class = $(this).attr("data-popup-close");
-    $(`[data-popup=${targeted_popup_class}`).fadeOut(350);
+    $(`[data-popup=${targeted_popup_class}]`).fadeOut(350);
     event.preventDefault();
 });
+
+/* For Google OAuth2 login */
+
+$(".login").click(function() {
+    $.ajax({
+        type: "GET",
+        url: "/login",
+        success: function(result) {
+            $(".login-popup").attr("data-token", result);
+        }
+    });
+});
+
+function start() {
+    gapi.load('auth2', function() {
+        auth2 = gapi.auth2.init({
+            client_id: "1023941705843-jhhhoait4klmlqamos53j3gd6q4dtito.apps.googleusercontent.com",
+        });
+    });
+}
+
+$("#signinButton").click(function(event) {
+    auth2.grantOfflineAccess().then(signInCallback);
+    event.preventDefault();
+});
+
+function signInCallback(authResult) {
+    if (authResult["code"]) {
+
+        // Hide the sign-in button now the user has been authorized.
+        //$("#signinButton").attr("style", "display: none");
+        state = $(".login-popup").attr("data-token");
+
+        // Send the one-time-use code and handle accordingly
+        $.ajax({
+            type: 'POST',
+            url: `/gconnect?state=${state}`,
+            processData: false,
+            data: authResult["code"],
+            contentType: 'application/octet-stream, charset=utf-8',
+            success: function(result) {
+                if (result) {
+                    $(".login-popup").fadeOut(150);
+                    $(".login > a").html("Logout")
+                }
+            }
+        });
+    } else if (authResult["error"]) {
+        console.log("There was an error: " + authResult["error"]);
+    } else {
+        console.log("Failed to make a server-side all. Check you configuration and console.");
+    }
+}
 
 $(".category").click(function() {
     // Would return all of the items of the clicked category 
